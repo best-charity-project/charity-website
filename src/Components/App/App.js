999520cd4efa1eea5f419390a59f60b4309433b7
Aliaksei Sauran <a.sauran@godeltech.com>
2018-02-02 14:39:20 +0300

    Add routing

---
+++ src/Components/App/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Header from '../Header/Header';
+import Navigation from '../Navigation/Navigation';
+import Admin from '../Admin/Admin';
+import About from '../About/About';
+import Home from '../Home/Home';
+import Footer from '../Footer/Footer';
+import News from '../News/News';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/admin" component={Admin}/>
+          <Route path="/about" component={About}/>
+          <Route path="/news" component={News}/>
+          <Redirect to="/"/>
+        </Switch>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                Logo here
+            </header>
+        );
+    }
+}
+
+export default Header
