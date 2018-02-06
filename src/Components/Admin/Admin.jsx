d16952c41cc96da111b96ecbedf2e8abad937315
Maryia Kukharchuk <m.kukharchuk@godeltech.com>
2018-02-06 19:40:51 +0300

    title of news component was changed

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import NewsList from './NewsList/NewsList.jsx';
+import NewsList from './NewsList/NewsListItem.jsx';
 export default () => (
     <div className="Admin">
         <NewsList title="Title" shortDescription='Description' />
-        <NewsList title="Title2" shortDescription='Description2' />
+        <NewsList title="Title2" shortDescription='Description2' />  
     </div>
 );
--- src/Components/Admin/NewsList/NewsListItem.jsx
+++ src/Components/Admin/NewsList/NewsListItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './newsList.css'
-class NewsList extends React.Component {
+import './newsListItem.css'
+class NewsListItem extends React.Component {
     render() {
         return (
             <li className="news-list">
