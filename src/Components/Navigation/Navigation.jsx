903a4811fcca740d2084591ceaffc13daef6d559
Aliaksei Husak <a.husak@godeltech.com>
2018-02-07 18:42:38 +0300

    changed menu size

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import classnames from 'classnames'; 
-import './nav.css';
+import classnames from 'classnames';
+import "./Navigation.css";
 
 class Navigation extends React.Component {
     constructor(props) {
