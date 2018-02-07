2836a250806693caa5c84eb3f4552d7b50328e08
Aliaksei Husak <a.husak@godeltech.com>
2018-02-07 14:39:11 +0300

    deleted classes, added functional component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,14 +2,8 @@ import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
-class Header extends React.Component {
-    render() {
-        return (
+export default () => (
             <header  className="header">
                 <Navigation />
             </header>
-        );
-    }
-}
-
-export default Header
+);
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import './Home.css';
 
-class Home extends React.Component {
-    render() {
-        return (
+export default () => (
             <div className = 'home indent'>
                 <div className='home--image-wrapper'>
                     <img src={require('../img/front.jpg')}  className='home--image_big'/>    
                 </div> 
             </div>
-        );
-    }
-}
-
-export default Home 
+);
