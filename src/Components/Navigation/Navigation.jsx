52db4d1dafaba5cec08d8ece0880fa8a45e09d8a
Aliaksei Husak <a.husak@godeltech.com>
2018-02-08 12:03:21 +0300

    rebamed some classes in navigation css

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -12,24 +12,20 @@ class Navigation extends React.Component {
         };
     }
     toggleClass() {
-        var body_style = window.getComputedStyle(document.body);
-        var body_width = parseInt(body_style.width, 10);
-        if (body_width < 500){
             const currentState = this.state.active;
             this.setState({ active: !currentState });
-        } 
     };
 
     render() {
-        const navigationClassNames = classnames('navigation--menu-image_close', {'navigation--menu-image_open' : this.state.active});
+        const navigationClassNames = classnames('navigation--menu-image', {'navigation--menu-open' : this.state.active});
         return (
             <nav className="navigation">
                 <div className='navigation--menu' onClick={this.toggleClass}>
                         <img className={navigationClassNames} src={require('../img/NavMenu.svg')}/>
-                        <Link to="/" className="navigation--menu-element_style">Home</Link>
-                        <Link to="/admin" className="navigation--menu-element_style">Admin</Link>
-                        <Link to="/about" className="navigation--menu-element_style">About</Link> 
-                        <Link to="/news" className="navigation--menu-element_style">News</Link>
+                        <Link to="/" className="navigation--menu-element">Home</Link>
+                        <Link to="/admin" className="navigation--menu-element">Admin</Link>
+                        <Link to="/about" className="navigation--menu-element">About</Link> 
+                        <Link to="/news" className="navigation--menu-element">News</Link>
                 </div>    
             </nav>
         );
