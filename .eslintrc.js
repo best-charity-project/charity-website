f39192dc3496c1b46c7025457ce089029c4116be
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-02-09 17:05:36 +0300

    deleted line from .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
-    extends: 'eslint-config-airbnb',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        sourceType: 'module',
+  extends: 'eslint-config-airbnb',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: ['react'],
-    rules: {
-        'jsx-a11y/anchor-is-valid': 0,
-        'jsx-quotes': ['error', 'prefer-single'],
-        'linebreak-style': [2, 'windows'],
-        'jsx-a11y/anchor-is-valid': 0,
-        'jsx-a11y/no-noninteractive-tabindex': 0,
-    },
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'jsx-a11y/anchor-is-valid': 0,
+    'jsx-quotes': ['error', 'prefer-single'],
+    'linebreak-style': [2, 'windows'],
+    'jsx-a11y/no-noninteractive-tabindex': 0,
+  },
 };
