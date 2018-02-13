609dcc03783ee54c876266a47fb14b92d4cbbd55
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-02-13 18:20:03 +0300

    added news list on admin page

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Form from './Form/Form';
 import './Admin.css';
+import NewsList from './NewsList/NewsList';
 
 export default () => (
   <div className='admin indent'>
     <Form />
+    <NewsList />
   </div>
 );
--- src/Components/Admin/NewsList/NewsListItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <li className='news-list-item'>
-    <p className='news-list-title'>{this.props.title}</p>
-    <p className='news-list--shortDescription'>{this.props.shortDescription}</p>
-    <button className='news-list--delete-button'>X</button>
-  </li>
-);
