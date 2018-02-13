ca75c41d3f345ab553378f137ce2ac4faadf2930
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-02-13 11:01:51 +0300

    created a function to shorten text

--- src/Components/News/SingleNews.jsx
+++ src/Components/News/SingleNews.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './SingleNews.css';
 
-export default props => (
-  <div className={props.className}>
-    <h2 className='single-news--title'>{props.title}</h2>
-    <p className='single-news--text'>{props.shortDescription}</p>
-    <Link to='/news/1' className='single-news-link'>
-      <button className='single-news--button'>Подробнее</button>
-    </Link>
-  </div>
-);
+export default class SingleNews extends React.Component {
+  static shortenText(short) {
+    let result = short;
+    const size = 240;
+    if (result.length > size) {
+      result = `${result.slice(0, size)} ...`;
+    }
+    return result;
+  }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className={this.props.className}>
+        <h2 className='single-news--title'>{this.props.title}</h2>
+        <p className='single-news--text'>{SingleNews.shortenText(this.props.shortDescription)}</p>
+        <Link to='/news/1' className='single-news-link'>
+          <button className='single-news--button'>Подробнее</button>
+        </Link>
+      </div>
+    );
+  }
+}
