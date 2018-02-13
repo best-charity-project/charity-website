510611758206ac6d6aca80d2a1354a816593ee55
Aliaksei Husak <a.husak@godeltech.com>
2018-02-13 15:24:05 +0300

    added eslint exceptions to database.js, index.js, beautife server.js, newsRoutes, delete registerServiceWorker

--- server/routes/newsRoutes.js
+++ server/routes/newsRoutes.js
@@ -1,31 +1,38 @@
 const express = require('express');
+
 const router = express.Router();
 const DB = require('../database/database');
+
 const database = new DB();
 
-router.route('/news')
-	.get((req, res) => {
-		database.connect(database.URI)
-			.then(() => {
-				return database.getAllNews();
-			})
-			.then(result => {
-				res.json(result);
-				database.close();
-			})
-			.catch(err => { throw err });
-	})
-	.post((req, res) => {
-		database.connect(database.URI)
-			.then(() => {
-				const news = req.body;
-				return database.addOneNews(news);
-			})
-			.then(() => {
-				res.json({ message: 'News was created successfully!' });
-				database.close();
-			})
-			.catch(err => { throw err });
-	});
+router
+  .route('/news')
+  .get((req, res) => {
+    database
+      .connect(database.URI)
+      .then(() => database.getAllNews())
+      .then((result) => {
+        res.json(result);
+        database.close();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  })
+  .post((req, res) => {
+    database
+      .connect(database.URI)
+      .then(() => {
+        const news = req.body;
+        return database.addOneNews(news);
+      })
+      .then(() => {
+        res.json({ message: 'News was created successfully!' });
+        database.close();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
 import App from './Components/App/App';
 import './index.css';
 
@@ -12,5 +11,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'), // eslint-disable-line no-undef
 );
-
-registerServiceWorker();
