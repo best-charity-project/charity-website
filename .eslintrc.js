9f90d015493d4e7f5e31813feff1b466eb10d802
Aliaksei Husak <a.husak@godeltech.com>
2018-02-14 17:56:30 +0300

    changed rules of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-    extends: ['eslint-config-airbnb'],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        sourceType: 'module',
+  extends: ['eslint-config-airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: ['react', 'json', 'html'],
-    rules: {
-        'jsx-a11y/anchor-is-valid': 0,
-        'jsx-quotes': ['error', 'prefer-single'],
-        'jsx-a11y/no-noninteractive-tabindex': 0,
-        'linebreak-style': 0,
-    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'json', 'html'],
+  rules: {
+    'jsx-a11y/anchor-is-valid': 0,
+    'jsx-quotes': ['error', 'prefer-single'],
+    'linebreak-style': 0,
+  },
 };
