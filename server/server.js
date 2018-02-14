286308e4d6ce0a2850fc81486ad4f8fa37bed176
Aliaksei Sauran <a.sauran@godeltech.com>
2018-02-14 18:37:52 +0300

    Add cors support in express

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ const router = require('./routes/newsRoutes');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = 8080;
+const port = process.env.PORT || 8080;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use('/api', router);
 
