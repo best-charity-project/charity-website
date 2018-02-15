b472aa057558f39bc4d2939f884fe63fc5702d6b
Aliaksei Sauran <a.sauran@godeltech.com>
2018-02-15 15:21:54 +0300

    Update Admin component to make post request

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import Form from './Form/Form';
 import './Admin.css';
-import NewsList from './NewsList/NewsList';
 
-export default () => (
-  <div className='admin indent'>
-    <Form />
-    <NewsList />
-  </div>
-);
+export default class Admin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNewsSubmit = this.handleNewsSubmit.bind(this);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleNewsSubmit(news) {
+    axios.post('https://charity-server.herokuapp.com/api/news', news).catch((err) => {
+      throw err;
+    });
+  }
+
+  render() {
+    return (
+      <div className='admin indent'>
+        <Form onNewsSubmit={this.handleNewsSubmit} />
+      </div>
+    );
+  }
+}
