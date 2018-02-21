1e9c7ac60da7e01a522d10e78e75589b7413ce00
Aliaksei Husak <a.husak@godeltech.com>
2018-02-21 20:25:39 +0300

    remove onKeyUp from navigation

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
@@ -36,6 +35,7 @@ class Navigation extends React.Component {
           <div
             className={this.getImageClass()}
             onClick={this.toggleClass}
+            onKeyUp=''
             tabIndex='0'
             role='button'
           >
