202e17d893a4cd256a7d2910b8ae492cbc25e8ea
Aliaksei Sauran <a.sauran@godeltech.com>
2018-02-21 19:17:37 +0300

    Refactor AdminNewsItem component

--- src/Components/Admin/AdminNewsItem.jsx
+++ src/Components/Admin/AdminNewsItem.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
-// import { getNews, addNews } from '../../newsCalls';
-// import Form from './Form/Form';
+import PropTypes from 'prop-types';
 import './Admin.css';
 import SingleNews from '../News/SingleNews/SingleNews';
 import ControlButton from '../ControlButton/ControlButton';
 import DetailsButton from '../DetailsButton/DetailsButton';
 
-export default class AdminNewsItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: this.props._id,
-      title: this.props.title,
-      shortDescription: this.props.shortDescription,
-      url: this.props.url,
-      date: this.props.date,
-    };
-    this.handleEdit = this.handleEdit.bind(this);
+const AdminNewsItem = (props) => {
+  function handleEdit() {
+    props.onNewsUpdate({
+      id: props._id,
+      title: props.title,
+      shortDescription: props.shortDescription,
+      url: props.url,
+      date: props.date,
+    });
   }
 
-  handleEdit() {
-    this.props.onNewsUpdate(this.state);
-  }
+  return (
+    <div className='news-list--item'>
+      <SingleNews title={props.title} shortDescription={props.shortDescription} />
+      <ControlButton text='Редактировать' onButtonClick={handleEdit} />
+      <ControlButton text='Удалить' onButtonClick={() => {}} />
+      <DetailsButton text='ПОДРОБНЕЕ' url='https://tut.by' />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className='news-list--item'>
-        <SingleNews title={this.props.title} shortDescription={this.props.shortDescription} />
-        <ControlButton text='Редактировать' onButtonClick={this.handleEdit} />
-        <ControlButton text='Удалить' />
-        <DetailsButton text='ПОДРОБНЕЕ' url='https://tut.by' />
-      </div>
-    );
-  }
-}
+export default AdminNewsItem;
+
+AdminNewsItem.propTypes = {
+  _id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  shortDescription: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  onNewsUpdate: PropTypes.func.isRequired,
+};
