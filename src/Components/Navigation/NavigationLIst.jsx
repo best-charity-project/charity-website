06b4da72bcc9bbc3fb8c7d8638f8d08fdf450bf3
Aliaksei Husak <a.husak@godeltech.com>
2018-02-26 14:54:31 +0300

    created new file for navigation pathways

--- src/Components/Navigation/NavigationLIst.jsx
+++ src/Components/Navigation/NavigationLIst.jsx
@@ -1,38 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import navigationElements from './NavigationPath.json';
 import './Navigation.css';
 
-const navigationElements = [
-  {
-    name: 'Главная',
-    path: '/',
-  },
-  {
-    name: 'Библиотека',
-    path: '/Library',
-  },
-  {
-    name: 'Новости',
-    path: '/news',
-  },
-  {
-    name: 'О нас',
-    path: '/about',
-  },
-  {
-    name: 'Admin',
-    path: '/admin',
-  },
-];
-
 function NavigationList(props) {
-  const listItems = navigationElements.map(item => (
+  return navigationElements.map(item => (
     <li key={item.path}>
       <Link onClick={props.onClick} to={item.path} className='navigation--element'>
         {item.name}
       </Link>
     </li>
   ));
-  return listItems;
 }
 export default NavigationList;
