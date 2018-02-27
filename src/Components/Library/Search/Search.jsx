9d461d227e34c2a80ad189d0729d4cf22c1bebe0
Maryia Kukharchuk <m.kukharchuk@godeltech.com>
2018-02-27 19:14:46 +0300

    search form was updated

--- src/Components/Library/Search/Search.jsx
+++ src/Components/Library/Search/Search.jsx
@@ -1,10 +1,92 @@
 import React from 'react';
+import './Search.css';
 
-export default () => (
-  <form role='search'>
-    <div>
-      <input type='search' id='search' placeholder='Поиск по категории...' />
-      <button>Поиск</button>
-    </div>
-  </form>
-);
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      checkedTypes: [],
+    };
+    this.handleChangeOfText = this.handleChangeOfText.bind(this);
+    this.handleChangeOfCheckboxes = this.handleChangeOfCheckboxes.bind(this);
+  }
+  handleChangeOfText(event) {
+    this.setState({ searchText: event.target.value });
+    console.log(this.state.searchText);
+  }
+  handleChangeOfCheckboxes(event) {
+    this.setState({ checkedTypes: [...this.state.checkedTypes, event.target.value] });
+    console.log(this.state.checkedTypes);
+  }
+  render() {
+    return (
+      <form role='search'>
+        <div>
+          <input
+            type='search'
+            id='search'
+            placeholder='Поиск по категории...'
+            value={this.state.searchText}
+            onChange={this.handleChangeOfText}
+          />
+          <button>Поиск</button>
+        </div>
+        <div className='search'>
+          <div>
+            <input
+              type='checkbox'
+              id='articles'
+              name='type'
+              value='articles'
+              onChange={this.handleChangeOfCheckboxes}
+            />
+            <label htmlFor='articles'>Статьи</label>
+          </div>
+          <div>
+            <input
+              type='checkbox'
+              id='video'
+              name='interest'
+              value='video'
+              onChange={this.handleChangeOfCheckboxes}
+            />
+            <label htmlFor='video'>Видео</label>
+          </div>
+          <div>
+            <input
+              type='checkbox'
+              id='literature'
+              name='interest'
+              value='literature'
+              onChange={this.handleChangeOfCheckboxes}
+            />
+            <label htmlFor='literature'>Литература</label>
+          </div>
+          <div>
+            <input
+              type='checkbox'
+              id='study_material'
+              name='interest'
+              value='study_material'
+              onChange={this.handleChangeOfCheckboxes}
+            />
+            <label htmlFor='study_material'>Учебный материал</label>
+          </div>
+          <div>
+            <input
+              type='checkbox'
+              id='all'
+              name='interest'
+              value='all'
+              onChange={this.handleChangeOfCheckboxes}
+            />
+            <label htmlFor='all'>Везде</label>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Search;
