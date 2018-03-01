f2ab56fbffbf7ad0de57ca09937d145af9177942
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-01 15:45:57 +0300

    Add routes to Library

--- src/Components/Library/Library.jsx
+++ src/Components/Library/Library.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { Route, Switch, Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Search from './Search/Search';
 import CategoriesList from './CategoriesList/CategoriesList';
 import Form from './Form/Form';
 import './Library.css';
 
-export default () => (
+const Library = ({ match }) => (
   <div className='library indent'>
     <h1 className='library--heading'>Библиотека</h1>
+    <Link to={`${match.url}/addToLibrary`} className='library--link'>
+      Добавить информацию в библиотеку
+    </Link>
+    <Link to={`${match.url}/categories`} className='library--link'>
+      Категории
+    </Link>
     <Search />
-    <CategoriesList />
-    <Form />
+    <Switch>
+      <Route exact path={`${match.url}/categories`} component={CategoriesList} />
+      <Route path={`${match.url}/addToLibrary`} component={Form} />
+      <Redirect to={`${match.url}/categories`} />
+    </Switch>
   </div>
 );
+
+export default Library;
+
+Library.propTypes = {
+  match: PropTypes.shape({
+    url: PropTypes.string,
+  }).isRequired,
+};
