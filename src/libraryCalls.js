2d34d79ccd01102b971e5c1299d4bde633ec9dbf
Maryia Kukharchuk <m.kukharchuk@godeltech.com>
2018-03-02 17:17:05 +0300

    sm fixies in form and librarycalls

--- src/libraryCalls.js
+++ src/libraryCalls.js
@@ -3,6 +3,6 @@ import API from './api';
 const getLibraryCategories = () => API.get('/categories').then(response => response.data);
 const getLibraryItems = () => API.get('/libraryItems').then(response => response.data);
 
-const addLibraryItem = libraryItem => API.post('libraryItemss', libraryItem);
+const addLibraryItem = libraryItem => API.post('libraryItems', libraryItem);
 
 export { getLibraryCategories, addLibraryItem, getLibraryItems };
