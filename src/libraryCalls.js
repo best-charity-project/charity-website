6d5ad9694401626728ec7f05beca49613888e393
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-03-02 13:38:04 +0300

    removed catching error

--- src/libraryCalls.js
+++ src/libraryCalls.js
@@ -1,15 +1,5 @@
 import API from './api';
 
-const getLibraryCategories = () =>
-  API.get('/Library/categories')
-    .then(response => response.data)
-    .catch((error) => {
-      throw error;
-    });
-const getLibraryItems = () =>
-  API.get('/libraryItems')
-    .then(response => response.data)
-    .catch((error) => {
-      throw error;
-    });
+const getLibraryCategories = () => API.get('/Library/categories').then(response => response.data);
+const getLibraryItems = () => API.get('/libraryItems').then(response => response.data);
 export { getLibraryCategories, getLibraryItems };
