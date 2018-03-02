b85f1096d52df30f13de8ad6043d64d3b8aa3e5f
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-03-02 13:58:04 +0300

    added padding

--- src/libraryCalls.js
+++ src/libraryCalls.js
@@ -1,5 +1,5 @@
 import API from './api';
 
-const getLibraryCategories = () => API.get('/Library/categories').then(response => response.data);
+const getLibraryCategories = () => API.get('/categories').then(response => response.data);
 const getLibraryItems = () => API.get('/libraryItems').then(response => response.data);
 export { getLibraryCategories, getLibraryItems };
