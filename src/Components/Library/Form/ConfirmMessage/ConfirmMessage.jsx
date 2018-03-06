f53fb10356d7973c942f15e1d248adc21a9df616
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-03-06 10:10:19 +0300

    added confirm message which appears after adding item to library

--- src/Components/Library/Form/ConfirmMessage/ConfirmMessage.jsx
+++ src/Components/Library/Form/ConfirmMessage/ConfirmMessage.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import './ConfirmMessage.css';
 
-const ConfirmMessage = () => (
-  <div className='confirm-message'>Документ был добавлен в библиотеку</div>
-);
+class ConfirmMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true,
+    };
+    this.setTimer = this.setTimer.bind(this);
+  }
+
+  componentDidMount() {
+    this.setTimer();
+  }
+
+  setTimer() {
+    setTimeout(() => {
+      this.setState({ visible: false });
+    }, 3000);
+  }
+  render() {
+    return this.state.visible ? (
+      <div className='confirm-message'>Документ был добавлен в библиотеку</div>
+    ) : (
+      <span />
+    );
+  }
+}
+
 export default ConfirmMessage;
