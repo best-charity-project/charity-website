9deb017215d8ffe6d54dbfc3fe4bdc87e54e3c4e
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-03-06 12:16:11 +0300

    renamed timer

--- src/Components/Library/Form/ConfirmMessage/ConfirmMessage.jsx
+++ src/Components/Library/Form/ConfirmMessage/ConfirmMessage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ConfirmMessage.css';
-import { timer } from '../../../../configs/config.json';
+import { confirmMessageTimer } from '../../../../configs/config.json';
 
 class ConfirmMessage extends React.Component {
   constructor(props) {
@@ -18,7 +18,7 @@ class ConfirmMessage extends React.Component {
   setTimer() {
     setTimeout(() => {
       this.setState({ visible: false });
-    }, timer);
+    }, confirmMessageTimer);
   }
 
   render() {
