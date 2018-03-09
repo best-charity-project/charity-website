a2a6f0137c275c1bcd8d21046fedd0e3f57b03d2
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-03-09 18:17:40 +0300

    added styles

--- src/Components/Admin/AdminLibrary/AdminLibraryList.jsx
+++ src/Components/Admin/AdminLibrary/AdminLibraryList.jsx
@@ -6,12 +6,10 @@ import LibraryItemsList from '../../Library/LibraryItemsList/LibraryItemsList';
 
 const AdminLibraryList = ({ match }) => (
   <div className='library indent'>
-    <div className='library--box'>
-      <Switch>
-        <Route exact path={`${match.url}`} component={CategoriesList} />
-        <Route path={`${match.url}/:category/:type`} component={LibraryItemsList} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path={`${match.url}`} component={CategoriesList} />
+      <Route path={`${match.url}/:category/:type`} component={LibraryItemsList} />
+    </Switch>
   </div>
 );
 
