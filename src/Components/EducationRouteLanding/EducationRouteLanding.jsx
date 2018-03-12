461a5e6fa6a77c6e2f1b6b22592fa5ef12ca45a6
Aliaksei Husak <a.husak@godeltech.com>
2018-03-12 15:22:52 +0300

    component EducationRouteCard become card and now it is generic

--- src/Components/EducationRouteLanding/EducationRouteLanding.jsx
+++ src/Components/EducationRouteLanding/EducationRouteLanding.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import EducationRouteCart from '../EducationRouteCard/EducationRouteCard';
+import Card from '../Card/Card';
 import educationImage from '../img/kids.svg';
 import './EducationRouteLanding.css';
 
 export default () => (
   <div className='education-route'>
     <img src={educationImage} alt='Образовательный маршрут' className='education-route--image' />
-    <EducationRouteCart />
+    <Card
+      heading='Образовательный маршрут'
+      text='Раздел образовательный маршрут предполагает возможность
+      объединение родителей для дальнейшего выстраивания образовательного маршрута
+      детей с особыми образовательными потребностями.'
+      path='education-route'
+    />
   </div>
 );
