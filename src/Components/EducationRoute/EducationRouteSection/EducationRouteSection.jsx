27f3077cc1a20979c7b13202932184ca9b205ee7
Aliaksei Husak <a.husak@godeltech.com>
2018-03-12 11:17:21 +0300

    extracted Education route cart to separate component

--- src/Components/EducationRoute/EducationRouteSection/EducationRouteSection.jsx
+++ src/Components/EducationRoute/EducationRouteSection/EducationRouteSection.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import educationRoute from './EducationRouteData.json';
+import EducationRouteCart from './EducationRouteCart/EducationRouteCart';
 import educationImage from '../../img/kids.svg';
 import './EducationRouteSection.css';
 
-export default () => educationRoute.map(item => (
-  <div className='home--education-route'>
+export default () => (
+  <div className='education-route'>
     <div className='education-route--image-wrapper' >
       <img src={educationImage} alt='Образовательный маршрут' className='education-route--image' />
     </div>
-    <div className='education-route--card'>
-      <h2 className='card--heading'>{item.header}</h2>
-      <p className='card--text'>{(item.text)}</p>
-      <Link to='education-route' className='education-route--link'>Перейти</Link>
-    </div>
+    <EducationRouteCart />
   </div>
-));
+);
--- src/Components/Library/LibraryDefault/LibraryDefault.jsx
+++ src/Components/Library/LibraryDefault/LibraryDefault.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import libraryPic from '../../img/bookshelf.svg';
 import './LibraryDefault.css';
 
-export default () => (
-  <div>
-    <img src={libraryPic} className='library--image-default' alt='Библиотека' />
-  </div>
-);
+export default () =>
+  <img src={libraryPic} className='library--image-default' alt='Библиотека' />;
+
