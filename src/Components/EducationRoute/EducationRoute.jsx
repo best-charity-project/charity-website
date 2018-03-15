29f4443274c4d8d8ec957b24bbf372c1a046ef74
Aliaksei Husak <a.husak@godeltech.com>
2018-03-15 17:12:36 +0300

    remove font open sans, now all fonts are roboto

--- src/Components/EducationRoute/EducationRoute.jsx
+++ src/Components/EducationRoute/EducationRoute.jsx
@@ -15,9 +15,9 @@ export default class Category extends React.Component {
       region: '',
       regionDistrict: '',
       city: '',
-      educationalInstitiution: '',
+      educationalInstitution: '',
       year: '',
-      programm: '',
+      program: '',
     };
     this.eddEducationRoute = this.eddEducationRoute.bind(this);
     this.setName = this.setName.bind(this);
@@ -26,9 +26,9 @@ export default class Category extends React.Component {
     this.setRegion = this.setRegion.bind(this);
     this.setRegionDistrict = this.setRegionDistrict.bind(this);
     this.setCity = this.setCity.bind(this);
-    this.setEducationalInstitiution = this.setEducationalInstitiution.bind(this);
+    this.seteducationalInstitution = this.seteducationalInstitution.bind(this);
     this.setYear = this.setYear.bind(this);
-    this.setProgramm = this.setProgramm.bind(this);
+    this.setprogram = this.setprogram.bind(this);
   }
 
   componentDidMount() {
@@ -37,9 +37,7 @@ export default class Category extends React.Component {
 
   setCategories() {
     getLocations().then((locations) => {
-      this.setState({
-        locations,
-      });
+      this.setState({ locations });
     });
   }
 
@@ -62,14 +60,16 @@ export default class Category extends React.Component {
   }
 
   setRegion(element) {
-    this.state.locationsIndex = element.target.value;
     this.setState({
-      districts: this.state.locations[this.state.locationsIndex].district,
+      districts: this.state.locations[element.target.value].district,
       region: this.state.locations[this.state.locationsIndex].name,
     });
-    if (element.target.value === '6') { this.state.city = 'Минск'; }
+    if (element.target.value === '6') {
+      this.setState({ city: 'Минск' });
+    } else {
+      this.setState({ city: '' });
+    }
   }
-
 
   setRegionDistrict(element) {
     this.setState({
@@ -83,9 +83,9 @@ export default class Category extends React.Component {
     });
   }
 
-  setEducationalInstitiution(element) {
+  seteducationalInstitution(element) {
     this.setState({
-      educationalInstitiution: element.target.value,
+      educationalInstitution: element.target.value,
     });
   }
 
@@ -95,16 +95,16 @@ export default class Category extends React.Component {
     });
   }
 
-  setProgramm(element) {
+  setprogram(element) {
     this.setState({
-      programm: element.target.value,
+      program: element.target.value,
     });
   }
 
   eddEducationRoute(event) {
     event.preventDefault();
     const {
-      name, phone, email, region, regionDistrict, city, educationalInstitiution, year, programm,
+      name, phone, email, region, regionDistrict, city, educationalInstitution, year, program,
     } = this.state;
     addEducation({
       name,
@@ -113,9 +113,20 @@ export default class Category extends React.Component {
       region,
       regionDistrict,
       city,
-      educationalInstitiution,
+      educationalInstitution,
       year,
-      programm,
+      program,
+    });
+    this.setState({
+      name: '',
+      phone: '',
+      email: '',
+      region: '',
+      regionDistrict: '',
+      city: '',
+      educationalInstitution: '',
+      year: '',
+      program: '',
     });
   }
 
@@ -123,77 +134,84 @@ export default class Category extends React.Component {
     return (
       <div className='education-route indent'>
         <h1 className='education-route--heading'>образовательный маршрут</h1>
-        <div className='education-route--form'>
-          <h2 className='form--title'>
+        <div className='education-route--education-form'>
+          <h2 className='education-form--title'>
             Анкета
           </h2>
-          <p className='form--description'>
-            <p>Уважаемые законные представители детей с особыми образовательными потребностями!</p>
-            <p>Предлагаем Вам заполнить анкету по определения образовательного маршрута.</p>
-            <p>Данная информация Вам поможет найти
+          <div className='education-form--description'>
+            <p>Уважаемые законные представители детей с
+              особыми образовательными потребностями!
+            </p>
+            <p>Предлагаем Вам заполнить анкету по
+              определения образовательного маршрута.
+            </p>
+            <p>Данная информация поможет Вам найти
               родителей в Республике Беларусь и объединиться.
             </p>
-          </p>
-          <form className='form' name='addName' onSubmit={this.eddEducationRoute}>
-            <div className='form--field-wrapper'>
-              <p className='form--field-comment'>
+          </div>
+          <form className='education-form' name='addName' onSubmit={this.eddEducationRoute}>
+            <div className='education-form--field-wrapper'>
+              <p className='education-form--field-comment'>
                 <label htmlFor='name'>Имя</label>
               </p>
               <input
+                value={this.state.name}
                 onChange={this.setName}
                 id='name'
                 type='text'
                 maxLength='40'
                 placeholder='Имя по которому к Вам можно обратиться'
-                className='form--field'
+                className='education-form--field'
                 pattern='^[А-Яа-яЁё\s]+$'
                 title='Имя должно содержать только буквы русского алфавита'
                 required
               />
               <span className='validity' />
             </div>
-            <div className='form--field-wrapper'>
-              <p className='form--field-comment'>
+            <div className='education-form--field-wrapper'>
+              <p className='education-form--field-comment'>
                 <label htmlFor='phone'>Номер телефона</label>
               </p>
               <input
+                value={this.state.phone}
                 onChange={this.setPhone}
                 id='phone'
                 pattern='[^А-Яа-яЁёA-Za-z]+$'
                 type='tel'
                 title='Введите корректный контактный телефон'
                 placeholder='+375-ХХ-ХХХ-ХХ-ХХ'
-                className='form--field'
+                className='education-form--field'
                 maxLength='20'
                 required
               />
               <span className='validity' />
             </div>
-            <div className='form--field-wrapper'>
-              <p className='form--field-comment'>
+            <div className='education-form--field-wrapper'>
+              <p className='education-form--field-comment'>
                 <label htmlFor='email'>Адрес электронной почты</label>
               </p>
               <input
+                value={this.state.email}
                 onChange={this.setEmail}
                 maxLength='35'
                 id='email'
                 type='email'
                 title='адрес электронной почты должен иметь формат: имя_почты@email.com'
                 placeholder='имя_почты@email.com'
-                className='form--field'
+                className='education-form--field'
                 pattern='.+@+*'
                 required
               />
               <span className='validity' />
             </div>
-            <div className='form--field-wrapper'>
-              <p className='form--field-comment'>
+            <div className='education-form--field-wrapper'>
+              <p className='education-form--field-comment'>
                 <label htmlFor='region'>Регион проживания</label>
               </p>
               <select
                 id='region'
                 title='Вы должны выбрать свой регион'
-                className='form--field'
+                className='education-form--field'
                 required
                 onChange={this.setRegion}
               >
@@ -206,14 +224,14 @@ export default class Category extends React.Component {
               </select>
               <span className='validity' />
             </div>
-            <div className='form--field-wrapper'>
-              <p className='form--field-comment'>
+            <div className='education-form--field-wrapper'>
+              <p className='education-form--field-comment'>
                 <label htmlFor='regionDistrict'>Областной район</label>
               </p>
               <select
                 id='regionDistrict'
                 title='Вы должны выбрать областной район'
-                className='form--field'
+                className='education-form--field'
                 onChange={this.setRegionDistrict}
                 required
               >
@@ -226,8 +244,8 @@ export default class Category extends React.Component {
               </select>
               <span className='validity' />
             </div>
-            <div className='form--field-wrapper'>
-              <p className='form--field-comment'>
+            <div className='education-form--field-wrapper'>
+              <p className='education-form--field-comment'>
                 <label htmlFor='city'>Город</label>
               </p>
               <input
@@ -238,21 +256,22 @@ export default class Category extends React.Component {
                 value={this.state.city}
                 title='Название города должно содержать только буквы русского алфавита'
                 placeholder='Ваш город'
-                className='form--field'
+                className='education-form--field'
                 pattern='^[А-Яа-яЁё\s]+$'
                 required
               />
               <span className='validity' />
             </div>
-            <div className='form--field-wrapper'>
-              <p className='form--field-comment'>
+            <div className='education-form--field-wrapper'>
+              <p className='education-form--field-comment'>
                 <label htmlFor='cityDistrict'>Выберите учреждение образования</label>
               </p>
               <select
                 id='regionDistrict'
-                onChange={this.setEducationalInstitiution}
+                value={this.state.educationalInstitution}
+                onChange={this.seteducationalInstitution}
                 title='Вы должны выбрать район города'
-                className='form--field'
+                className='education-form--field'
                 required
               >
                 <option value='' disabled selected>
@@ -263,38 +282,39 @@ export default class Category extends React.Component {
               </select>
               <span className='validity' />
             </div>
-            <div className='form--field-wrapper'>
-              <p className='form--field-comment'>
+            <div className='education-form--field-wrapper'>
+              <p className='education-form--field-comment'>
                 <label htmlFor='year'>В каком году учащийся пойдет в учреждение образования</label>
               </p>
               <input
                 id='year'
+                value={this.state.year}
                 onChange={this.setYear}
                 type='number'
                 title='дата должна содержать только год и начинаться с 20..'
                 placeholder='20__'
-                className='form--field'
+                className='education-form--field'
                 min='2018'
                 max='2100'
                 required
               />
               <span className='validity' />
             </div>
-            <div className='form--field-wrapper'>
-              <p className='form--field-comment'>
+            <div className='education-form--field-wrapper'>
+              <p className='education-form--field-comment'>
                 <label htmlFor='name'>Рекомендованная программа образования</label>
               </p>
               <input
                 id='name'
-                onChange={this.setProgramm}
+                value={this.state.program}
+                onChange={this.setprogram}
                 type='text'
-                maxLength='40'
+                maxLength='80'
                 placeholder='Программа обучения рекомендованная ЦКРОиР'
-                className='form--field'
-                pattern='^[А-Яа-яЁё\s]+$'
+                className='education-form--field'
               />
             </div>
-            <div className='form--field-wrapper'>
+            <div className='education-form--field-wrapper'>
               <p><span>*</span> - поля, обязательные для заполнения</p>
             </div>
             <input
