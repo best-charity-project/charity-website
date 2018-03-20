48d1cbb6ed0763aaa01d7766c17ee6e1bdfdbdce
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-20 18:10:17 +0300

    Add signup page controller

---
+++ src/Components/Signup/SignupPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import SignupForm from './SignupForm';
+import { signupUser } from '../../Auth/Auth';
+import './SignupForm.css';
+
+class SignupPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      successMessage: '',
+      errorMessage: '',
+    };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit(formData) {
+    signupUser(formData).then((res) => {
+      if (res.data.error) {
+        this.setState({ errorMessage: res.data.error });
+        return;
+      }
+      this.setState({ successMessage: res.data.message });
+    });
+  }
+
+  render() {
+    return (
+      <div className='indent'>
+        <SignupForm onSubmit={this.handleFormSubmit} />
+        <p className='signup--success-message'>{this.state.successMessage}</p>
+        <p className='signup--error-message'>{this.state.errorMessage}</p>
+      </div>
+    );
+  }
+}
+
+export default SignupPage;
