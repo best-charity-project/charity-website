d3096bf3f1d95b3ec393513bdbf45d4a0aebf111
Maryia Kukharchuk <m.kukharchuk@godeltech.com>
2018-03-21 16:26:07 +0300

    move librarysearchlist in same component as libraryitemslist

--- src/Components/Library/Library.jsx
+++ src/Components/Library/Library.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Search from './Search/Search';
 import LibraryItemsList from './LibraryItemsList/LibraryItemsList';
 import CategoriesList from './CategoriesList/CategoriesList';
-import LibrarySearchList from './LibrarySearchList/LibrarySearchList';
 import AddLibraryItem from './Form/AddLibraryItem';
 import LibraryDefault from './LibraryDefault/LibraryDefault';
 import './Library.css';
@@ -24,7 +23,7 @@ const Library = ({ match }) => (
         </div>
         <Switch>
           <Route exact path={`${match.url}/`} component={LibraryDefault} />
-          <Route path={`${match.url}/search`} component={LibrarySearchList} />
+          <Route path={`${match.url}/search`} component={LibraryItemsList} />
           <Route path={`${match.url}/addToLibrary`} component={AddLibraryItem} />
           <Route path={`${match.url}/:category/:type`} component={LibraryItemsList} />
           <Redirect to={`${match.url}/`} />
--- src/Components/Library/LibraryItemsList/LibraryItemsList.jsx
+++ src/Components/Library/LibraryItemsList/LibraryItemsList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getLibraryItems } from '../../../libraryCalls';
+import URLSearchParams from 'url-search-params';
+import { fullTextLibrarySearch, getLibraryItems } from '../../../libraryCalls';
 import LibraryItem from '../LibraryItem/LibraryItem';
 import DetailsButton from '../../DetailsButton/DetailsButton';
 
@@ -11,15 +12,25 @@ class LibraryItemsList extends React.Component {
       libraryItems: [],
     };
   }
-
   componentDidMount() {
-    this.setLibraryItems(this.props.match.params);
+    if (this.props.location.search) {
+      this.setSearchTextResult(this.props.location.search);
+    } else this.setLibraryItems(this.props.match.params);
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.location.pathname !== nextProps.location.pathname) {
+    if (nextProps.location.search) {
+      this.setSearchTextResult(nextProps.location.search);
+    } else if (this.props.location.pathname !== nextProps.location.pathname) {
       this.setLibraryItems(nextProps.match.params);
     }
+  }
+
+  setSearchTextResult(urlParams) {
+    const urlParamsQuery = new URLSearchParams(urlParams);
+    const textSearch = encodeURIComponent(urlParamsQuery.get('textSearch'));
+    const types = urlParamsQuery.get('types');
+    fullTextLibrarySearch(textSearch, types).then(libraryItems => this.setState({ libraryItems }));
   }
 
   setLibraryItems({ category, type }) {
@@ -27,32 +38,30 @@ class LibraryItemsList extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        {this.state.libraryItems.map(item => (
-          <div key={item._id} className='library-item'>
-            <LibraryItem {...item} />
-            <DetailsButton
-              className='control-button control-button--blue'
-              text='Подробнее'
-              url={item.url}
-            />
-          </div>
-        ))}
+    return this.state.libraryItems.map(item => (
+      <div key={item._id} className='library-item'>
+        <LibraryItem {...item} />
+        <DetailsButton
+          className='control-button control-button--blue'
+          text='Подробнее'
+          url={item.url}
+        />
       </div>
-    );
+    ));
   }
 }
 
 export default LibraryItemsList;
+
 LibraryItemsList.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired,
+    search: PropTypes.string.isRequired,
   }).isRequired,
   match: PropTypes.shape({
     params: PropTypes.shape({
-      category: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
+      category: PropTypes.string,
+      type: PropTypes.string,
     }),
   }).isRequired,
 };
