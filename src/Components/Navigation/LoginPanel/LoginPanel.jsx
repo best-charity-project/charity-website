c877ecdb0d8ce0f23bcf84028f1aefa92e95af59
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-21 12:46:20 +0300

    Move login button from navigation to LoginPanel

--- src/Components/Navigation/LoginPanel/LoginPanel.jsx
+++ src/Components/Navigation/LoginPanel/LoginPanel.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ControlButton from '../../ControlButton/ControlButton';
 import './LoginPanel.css';
 
 const LoginPanel = ({ userInfo, onLogout }) => {
   if (!userInfo.name) {
-    return null;
+    return (
+      <div className='login-panel'>
+        <Link to='/login' className='login-panel--link'>
+          Войти
+        </Link>
+      </div>
+    );
   }
   return (
     <div className='login-panel'>
-      <h1 className='login-panel--heading'>Привет, {userInfo.name}</h1>
-      <ControlButton onButtonClick={onLogout} text='Выйти' className='login-panel--logout-btn' />
+      <h1 className='login-panel--heading'>{userInfo.name}</h1>
+      <ControlButton onButtonClick={onLogout} text='Выйти' className='login-panel--btn' />
     </div>
   );
 };
