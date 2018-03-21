ac3da73dca630f031f77ad66b89b2aa27922e116
Aliaksei Sauran <a.sauran@godeltech.com>
2018-03-21 11:08:25 +0300

    Refactor Admin to stateless component

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -6,77 +6,41 @@ import AdminNews from './AdminNews';
 import EditNews from './AddEditNews/EditNews';
 import AddNews from './AddEditNews/AddNews';
 import AdminLibrary from './AdminLibrary';
-import { isUserAuthenticated } from '../../Auth/Auth';
 
-class Admin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isAdmin: false,
-    };
-  }
-
-  componentDidMount() {
-    isUserAuthenticated()
-      .then((userInfo) => {
-        if (userInfo.admin) {
-          this.setState({ isAdmin: true });
-        } else {
-          this.props.history.push('/home');
-        }
-      })
-      .catch((err) => {
-        if (err) {
-          this.props.history.push('/home');
-        }
-      });
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (!nextProps.userInfo.admin) {
-      this.props.history.push('/home');
-    }
-    this.setState({ isAdmin: nextProps.userInfo.admin });
-  }
-
-  render() {
-    return (
-      <div className='admin indent'>
-        {this.state.isAdmin && (
-          <div className='admin--box'>
-            <div className='admin--sidebar'>
-              <Link to={`${this.props.match.url}/news`} className='admin--link'>
-                Посмотреть все новости
-              </Link>
-              <Link to={`${this.props.match.url}/addNews`} className='admin--link'>
-                Добавить новость
-              </Link>
-              <Link to={`${this.props.match.url}/library`} className='admin--link'>
-                Библиотека
-              </Link>
-            </div>
-            <Switch>
-              <Route exact path={`${this.props.match.url}/news`} component={AdminNews} />
-              <Route path={`${this.props.match.url}/addNews`} component={AddNews} />
-              <Route path={`${this.props.match.url}/news/edit/:id`} component={EditNews} />
-              <Route path={`${this.props.match.url}/library`} component={AdminLibrary} />
-              <Redirect to={`${this.props.match.url}/news`} />
-            </Switch>
-          </div>
-        )}
+const Admin = ({ match, userInfo }) => (
+  <div className='admin indent'>
+    {userInfo.admin && (
+      <div className='admin--box'>
+        <div className='admin--sidebar'>
+          <Link to={`${match.url}/news`} className='admin--link'>
+            Посмотреть все новости
+          </Link>
+          <Link to={`${match.url}/addNews`} className='admin--link'>
+            Добавить новость
+          </Link>
+          <Link to={`${match.url}/library`} className='admin--link'>
+            Библиотека
+          </Link>
+        </div>
+        <Switch>
+          <Route exact path={`${match.url}/news`} component={AdminNews} />
+          <Route path={`${match.url}/addNews`} component={AddNews} />
+          <Route path={`${match.url}/news/edit/:id`} component={EditNews} />
+          <Route path={`${match.url}/library`} component={AdminLibrary} />
+          <Redirect to={`${match.url}/news`} />
+        </Switch>
       </div>
-    );
-  }
-}
+    )}
+    {!userInfo.admin && <Redirect to='/home' />}
+    {!userInfo.admin === undefined && <h1>Загрузка...</h1>}
+  </div>
+);
 
 export default withRouter(Admin);
 
 Admin.propTypes = {
   match: PropTypes.shape({
     url: PropTypes.string,
-  }).isRequired,
-  history: PropTypes.shape({
-    push: PropTypes.func,
   }).isRequired,
   userInfo: PropTypes.shape({
     admin: PropTypes.bool,
