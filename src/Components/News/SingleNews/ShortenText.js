f64b3f0b49f1f765638090526863248f57e64fa1
Aliaksei Husak <a.husak@godeltech.com>
2018-03-21 14:52:09 +0300

    fixed problem with not result in shortenText function

--- src/Components/News/SingleNews/ShortenText.js
+++ src/Components/News/SingleNews/ShortenText.js
@@ -1,6 +1,10 @@
 function shortenText(short) {
   let result = short;
   const size = 240;
+  if (!result) {
+    return '';
+  }
+
   if (result.length > size) {
     result = `${result.slice(0, size)} ...`;
   }
