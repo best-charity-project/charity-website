52cce2592cd8bd67ae3b0ed1d09f01fdb2adbe96
Maryia Kukharchuk <m.kukharchuk@godeltech.com>
2018-03-26 17:33:09 +0300

    add forum link

--- src/Components/Navigation/NavigationList.jsx
+++ src/Components/Navigation/NavigationList.jsx
@@ -3,10 +3,11 @@ import { NavLink } from 'react-router-dom';
 import navigationPaths from './NavigationPaths.json';
 import './Navigation.css';
 
-export default props => navigationPaths.map(item => (
-  <li key={item.path}>
-    <NavLink onClick={props.onClick} to={item.path} className='navigation--element'>
-      {item.name}
-    </NavLink >
-  </li>
-));
+export default props =>
+  navigationPaths.map(item => (
+    <li key={item.path}>
+      <NavLink onClick={props.onClick} to={item.path} className='navigation--element'>
+        {item.name}
+      </NavLink>
+    </li>
+  ));
