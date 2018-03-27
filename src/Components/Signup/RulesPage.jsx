90da1513f98f7d0966d01b4acb42843a7ac9741e
Olga Yakimovich <v.yakimovich@godeltech.com>
2018-03-27 09:33:48 +0300

    changed json file, removed comment from svg file

--- src/Components/Signup/RulesPage.jsx
+++ src/Components/Signup/RulesPage.jsx
@@ -7,7 +7,7 @@ const RulesPage = props => (
   <div className='form--accept-rules'>
     <div className='accept-rules--window'>
       <button className='window--close' onClick={props.toggle} />
-      <p className='window--heading'>{rules[0]}</p>
+      <p className='window--heading'>{rules[0].title}</p>
       {rules.slice(1).map(rule => <p key={rule.slice(0, 20)}>{rule}</p>)}
     </div>
   </div>
