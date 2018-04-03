9ebea852e3cb8fd587da1b39b6604c1e921aa39b
Aliaksei Sauran <a.sauran@godeltech.com>
2018-04-03 18:35:33 +0300

    Add change password request

--- src/accountCalls.js
+++ src/accountCalls.js
@@ -1,6 +1,12 @@
 import API from './api';
+import { getToken } from './Auth/Auth';
 
 const restorePassword = email =>
   API.post('account/restore-password', { email }).then(response => response.data);
 
-export default restorePassword;
+const changePassword = data =>
+  API.post('account/change-password', data, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  }).then(response => response.data);
+
+export { restorePassword, changePassword };
