f1cbbed5427892f1d5f25f554fa791d60ea9526a
Aliaksei Husak <a.husak@godeltech.com>
2018-04-04 18:52:23 +0300

    removed useless states from Education Route card

--- src/Components/CompletedEducationRouteForm/CompletedEducationRouteForm.jsx
+++ src/Components/CompletedEducationRouteForm/CompletedEducationRouteForm.jsx
@@ -25,7 +25,7 @@ export default class CompletedEducationRouteForm extends React.Component {
         <h2 className='secondary-heading'>Заполненные карты</h2>
         <div className='education-route--user-cards'>
           {this.state.usersCarts.map((item, index) => (
-            <EducationRouteCard index={index} key={item._id} {...item} {...this.state.isOpen} />
+            <EducationRouteCard index={index} key={item._id} {...item} />
           ))}
         </div>
       </div>
