92ff923ec19201d26ebcba17c8db4b15403c4df3
Aliaksei Sauran <a.sauran@godeltech.com>
2018-04-06 11:58:13 +0300

    Create form to change password for not logged in users

---
+++ src/Components/ChangeForgotenPasswordPage/ChangeForgotenPasswordForm.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ChangeForgotenPasswordForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      confirmPassword: '',
+      newPassword: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(this);
+    this.handleNewPasswordChange = this.handleNewPasswordChange.bind(this);
+  }
+
+  handleConfirmPasswordChange() {
+    this.setState({ confirmPassword: this.confirmPassword.value });
+    if (this.newPassword.value !== this.confirmPassword.value) {
+      this.confirmPassword.setCustomValidity('Пароль не соответствует');
+    } else {
+      this.confirmPassword.setCustomValidity('');
+    }
+  }
+
+  handleNewPasswordChange(e) {
+    this.setState({
+      newPassword: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { newPassword } = this.state;
+    this.props.onSubmit({ newPassword });
+  }
+
+  render() {
+    return (
+      <div className='changePasswordForm form'>
+        <form name='changePasswordForm' onSubmit={this.handleSubmit}>
+          <div className='form--box'>
+            <input
+              id='newPassword'
+              value={this.state.newPassword}
+              onChange={this.handleNewPasswordChange}
+              type='password'
+              className={this.state.newPassword ? 'form--field label-move-up' : 'form--field'}
+              minLength='6'
+              ref={(input) => {
+                this.newPassword = input;
+              }}
+              required
+            />
+            <label htmlFor='newPassword' className='form--placeholder'>
+              Новый пароль
+            </label>
+          </div>
+          <div className='form--box'>
+            <input
+              id='confirmPassword'
+              value={this.state.confirmPassword}
+              onChange={this.handleConfirmPasswordChange}
+              type='password'
+              className={this.state.confirmPassword ? 'form--field label-move-up' : 'form--field'}
+              minLength='6'
+              ref={(input) => {
+                this.confirmPassword = input;
+              }}
+              required
+            />
+            <label htmlFor='confirmPassword' className='form--placeholder'>
+              Подтвердите пароль
+            </label>
+          </div>
+          <input
+            type='submit'
+            className='control-button control-button--blue '
+            value={this.props.buttonText}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ChangeForgotenPasswordForm;
+
+ChangeForgotenPasswordForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  buttonText: PropTypes.string.isRequired,
+};
