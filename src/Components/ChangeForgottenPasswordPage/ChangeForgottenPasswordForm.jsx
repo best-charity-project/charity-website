5627b8f6186c9acda5a937837475f82418a17294
Aliaksei Sauran <a.sauran@godeltech.com>
2018-04-10 15:45:29 +0300

    Change password forms refactoring

--- src/Components/ChangeForgottenPasswordPage/ChangeForgottenPasswordForm.jsx
+++ src/Components/ChangeForgottenPasswordPage/ChangeForgottenPasswordForm.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { minPasswordLength } from '../../configs/config.json';
-import confirmPassword from '../Form/confirmPassword';
+import PasswordInput from '../PasswordInput/PasswordInput';
+import ConfirmPassword from '../ConfirmPassword/ConfirmPassword';
 
 export default class ChangeForgotenPasswordForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      confirmPassword: '',
       newPassword: '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(this);
     this.handleNewPasswordChange = this.handleNewPasswordChange.bind(this);
   }
 
-  handleConfirmPasswordChange() {
-    this.setState({ confirmPassword: this.confirmPassword.value });
-    confirmPassword(this.newPassword, this.confirmPassword);
-  }
-
-  handleNewPasswordChange(e) {
+  handleNewPasswordChange(newPassword) {
     this.setState({
-      newPassword: e.target.value,
+      newPassword,
     });
   }
 
@@ -36,40 +29,16 @@ export default class ChangeForgotenPasswordForm extends React.Component {
     return (
       <div className='changePasswordForm form'>
         <form name='changePasswordForm' onSubmit={this.handleSubmit}>
-          <div className='form--box'>
-            <input
-              id='newPassword'
-              value={this.state.newPassword}
-              onChange={this.handleNewPasswordChange}
-              type='password'
-              className={this.state.newPassword ? 'form--field label-move-up' : 'form--field'}
-              minLength={minPasswordLength}
-              ref={(input) => {
-                this.newPassword = input;
-              }}
-              required
-            />
-            <label htmlFor='newPassword' className='form--placeholder'>
-              Новый пароль
-            </label>
-          </div>
-          <div className='form--box'>
-            <input
-              id='confirmPassword'
-              value={this.state.confirmPassword}
-              onChange={this.handleConfirmPasswordChange}
-              type='password'
-              className={this.state.confirmPassword ? 'form--field label-move-up' : 'form--field'}
-              minLength={minPasswordLength}
-              ref={(input) => {
-                this.confirmPassword = input;
-              }}
-              required
-            />
-            <label htmlFor='confirmPassword' className='form--placeholder'>
-              Подтвердите пароль
-            </label>
-          </div>
+          <PasswordInput
+            id='newPassword'
+            onChange={this.handleNewPasswordChange}
+            labelText='Новый пароль'
+          />
+          <ConfirmPassword
+            id='confirmPassword'
+            newPassword={this.state.newPassword}
+            labelText='Подтвердите пароль'
+          />
           <input
             type='submit'
             className='control-button control-button--blue'
--- src/Components/ChangePasswordPage/ChangePasswordForm.jsx
+++ src/Components/ChangePasswordPage/ChangePasswordForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import confirmPassword from '../Form/confirmPassword';
-import { minPasswordLength } from '../../configs/config.json';
+import PasswordInput from '../PasswordInput/PasswordInput';
+import ConfirmPassword from '../ConfirmPassword/ConfirmPassword';
 import './ChangePasswordForm.css';
 
 export default class ChangePasswordForm extends React.Component {
@@ -9,29 +9,22 @@ export default class ChangePasswordForm extends React.Component {
     super(props);
     this.state = {
       oldPassword: '',
-      confirmPassword: '',
       newPassword: '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleConfirmPasswordChange = this.handleConfirmPasswordChange.bind(this);
     this.handleNewPasswordChange = this.handleNewPasswordChange.bind(this);
     this.handleOldPasswordChange = this.handleOldPasswordChange.bind(this);
   }
 
-  handleConfirmPasswordChange() {
-    this.setState({ confirmPassword: this.confirmPassword.value });
-    confirmPassword(this.newPassword, this.confirmPassword);
-  }
-
-  handleOldPasswordChange(e) {
+  handleOldPasswordChange(oldPassword) {
     this.setState({
-      oldPassword: e.target.value,
+      oldPassword,
     });
   }
 
-  handleNewPasswordChange(e) {
+  handleNewPasswordChange(newPassword) {
     this.setState({
-      newPassword: e.target.value,
+      newPassword,
     });
   }
 
@@ -45,54 +38,21 @@ export default class ChangePasswordForm extends React.Component {
     return (
       <div className='changePasswordForm form'>
         <form name='changePasswordForm' onSubmit={this.handleSubmit}>
-          <div className='form--box'>
-            <input
-              id='oldPassword'
-              value={this.state.oldPassword}
-              onChange={this.handleOldPasswordChange}
-              type='password'
-              className={this.state.oldPassword ? 'form--field label-move-up' : 'form--field'}
-              minLength={minPasswordLength}
-              required
-            />
-            <label htmlFor='oldPassword' className='form--placeholder'>
-              Старый пароль
-            </label>
-          </div>
-          <div className='form--box'>
-            <input
-              id='newPassword'
-              value={this.state.newPassword}
-              onChange={this.handleNewPasswordChange}
-              type='password'
-              className={this.state.newPassword ? 'form--field label-move-up' : 'form--field'}
-              minLength={minPasswordLength}
-              ref={(input) => {
-                this.newPassword = input;
-              }}
-              required
-            />
-            <label htmlFor='newPassword' className='form--placeholder'>
-              Новый пароль
-            </label>
-          </div>
-          <div className='form--box'>
-            <input
-              id='confirmPassword'
-              value={this.state.confirmPassword}
-              onChange={this.handleConfirmPasswordChange}
-              type='password'
-              className={this.state.confirmPassword ? 'form--field label-move-up' : 'form--field'}
-              minLength={minPasswordLength}
-              ref={(input) => {
-                this.confirmPassword = input;
-              }}
-              required
-            />
-            <label htmlFor='confirmPassword' className='form--placeholder'>
-              Подтвердите пароль
-            </label>
-          </div>
+          <PasswordInput
+            id='oldPassword'
+            onChange={this.handleOldPasswordChange}
+            labelText='Старый пароль'
+          />
+          <PasswordInput
+            id='newPassword'
+            onChange={this.handleNewPasswordChange}
+            labelText='Новый пароль'
+          />
+          <ConfirmPassword
+            id='confirmPassword'
+            newPassword={this.state.newPassword}
+            labelText='Подтвердите пароль'
+          />
           <input
             type='submit'
             className='control-button control-button--blue '
