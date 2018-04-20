8113a8980ce0fefa9b8997c96cd04bd9cdb6327e
Aliaksei Husak <a.husak@godeltech.com>
2018-04-20 16:53:57 +0300

    renamed classes in calendar event

--- src/Components/CalendarEvent/CalendarEvent.jsx
+++ src/Components/CalendarEvent/CalendarEvent.jsx
@@ -9,19 +9,18 @@ const CalendarEvent = ({
   title, description, url, start, end,
 }) => (
   <div className='calendar-event'>
-    <h2 className='event--title'>{title}</h2>
-    <p className='event--description'>{shortenText(description)}</p>
-    <div className='event--time-wrapper'>
-      <div className='time--box'>
-        <p className='time--box--value'>Когда: {moment(start).format('LL')}</p>
+    <h2 className='calendar-event--title'>{title}</h2>
+    <p className='calendar-event--description'>{shortenText(description)}</p>
+    <div className='calendar-event--time-wrapper'>
+      <div className='time-box'>
+        <p className='time-box--value'>Когда: {moment(start).format('LL')}</p>
       </div>
-      <div className='time--box'>
-        <p className='event--time'>Время начала: {moment(start).format('HH:mm')}</p>
-        <p className='event--time'>Время окончания: {moment(end).format('HH:mm')}</p>
+      <div className='time-box'>
+        <p className='time-box--value'>Время начала: {moment(start).format('HH:mm')}</p>
+        <p className='time-box--value'>Время окончания: {moment(end).format('HH:mm')}</p>
       </div>
     </div>
-
-    <Link to={url} className='event--link' rel='noopener noreferrer'>
+    <Link to={url} className='calendar-event--link' rel='noopener noreferrer'>
       подробнее
     </Link>
   </div>
