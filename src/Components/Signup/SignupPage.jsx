e86235287da056a028d0eafa2a3086873feef31f
Aliaksei Sauran <a.sauran@godeltech.com>
2018-05-14 11:35:14 +0300

    Remove message dublication

--- src/Components/Signup/SignupPage.jsx
+++ src/Components/Signup/SignupPage.jsx
@@ -3,57 +3,30 @@ import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import SignupForm from './SignupForm';
 import { signupUser } from '../../Auth/Auth';
-import Message from '../Message/Message';
-import { redirectTime } from '../../configs/config.json';
-import createMessage from '../Message/createMessage';
 import './SignupForm.css';
 
-class SignupPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: {
-        type: '',
-        text: '',
-      },
-    };
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    this.redirect = this.redirect.bind(this);
+const SignupPage = ({ showMessage, onAuthChange }) => {
+  function handleFormSubmit(formData) {
+    signupUser(formData)
+      .then((data) => {
+        showMessage({ type: 'success', text: data.message });
+        onAuthChange(data.userInfo);
+        this.redirect();
+      })
+      .catch((err) => {
+        this.props.showMessage({ type: 'error', text: err.response.data.message });
+      });
   }
-
-  redirect() {
-    setTimeout(() => {
-      this.props.history.push('/home');
-    }, redirectTime);
-  }
-
-  handleFormSubmit(formData) {
-    signupUser(formData).then((data) => {
-      if (data.error) {
-        this.setState({ message: createMessage('error', data.error) });
-        return;
-      }
-      this.setState({ message: createMessage('success', data.message) });
-      this.props.onAuthChange(data.userInfo);
-      this.redirect();
-    });
-  }
-
-  render() {
-    return (
-      <div className='indent'>
-        <Message {...this.state.message} />
-        <SignupForm onSubmit={this.handleFormSubmit} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className='indent'>
+      <SignupForm onSubmit={handleFormSubmit} />
+    </div>
+  );
+};
 
 export default withRouter(SignupPage);
 
 SignupPage.propTypes = {
   onAuthChange: PropTypes.func.isRequired,
-  history: PropTypes.shape({
-    push: PropTypes.func,
-  }).isRequired,
+  showMessage: PropTypes.func.isRequired,
 };
