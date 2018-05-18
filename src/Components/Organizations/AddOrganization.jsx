a5c1dbd104f96af79c071abfd38e456f98596b15
Aliaksei Sauran <aleks171@hotmail.com>
2018-05-18 17:44:21 +0300

    AddOrganization component refactoring

--- src/Components/Organizations/AddOrganization.jsx
+++ src/Components/Organizations/AddOrganization.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import OrganizationsForm from './OrganizationsForm';
 import { addOrganization } from '../../organizationsCalls';
+import Message from '../Message/Message';
 
-const AddOrganization = ({ showMessage }) => {
-  function handleFormSubmit(formData) {
+class AddOrganization extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: {
+        type: '',
+        text: '',
+      },
+    };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit(formData) {
     addOrganization(formData)
       .then((data) => {
-        showMessage({ type: 'success', text: data.message });
+        this.setState({ message: { type: 'success', text: data.message } });
       })
       .catch((err) => {
-        showMessage({ type: 'error', text: err.response.data.message });
+        this.setState({ message: { type: 'error', text: err.response.data.message } });
       });
   }
 
-  return (
-    <div>
-      <OrganizationsForm onSubmit={handleFormSubmit} />
-      <p className='add-organization-item-comment'>
-        *** Информация отобразится в разделе после одобрения модератором
-      </p>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <Message {...this.state.message} />
+        <OrganizationsForm onSubmit={this.handleFormSubmit} />
+        <p className='add-organization-item-comment'>
+          *** Информация отобразится в разделе после одобрения модератором
+        </p>
+      </div>
+    );
+  }
+}
 
-export default withRouter(AddOrganization);
-
-AddOrganization.propTypes = {
-  showMessage: PropTypes.func.isRequired,
-};
+export default AddOrganization;
