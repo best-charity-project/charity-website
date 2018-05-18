214257cb662df9d7e0651514773a4f7f5f1b0edc
Aliaksei Sauran <aleks171@hotmail.com>
2018-05-18 17:45:43 +0300

    OrganizationsPage component fix

--- src/Components/Organizations/OrganizationsPage.jsx
+++ src/Components/Organizations/OrganizationsPage.jsx
@@ -4,21 +4,24 @@ import PropTypes from 'prop-types';
 import ListOfOrganizations from './ListOfOrganizations';
 import AddOrganization from './AddOrganization';
 import RedirectToAuthorization from '../RedirectToAuthorization/RedirectToAuthorization';
+import './OrganizationsPage.css';
 
 const OrganizationsPage = ({ match, userInfo, showMessage }) => (
   <div className='organizations indent'>
-    <Switch>
-      <Route
-        exact
-        path={`${match.url}/`}
-        render={() => <ListOfOrganizations userInfo={userInfo.name} match={match} />}
-      />
-      <Route
-        path={`${match.url}/addOrganization`}
-        render={() => <AddOrganization showMessage={showMessage} />}
-      />
-      <Route path={`${match.url}/login`} component={RedirectToAuthorization} />
-    </Switch>
+    <div className='organizations--box'>
+      <Switch>
+        <Route
+          exact
+          path={`${match.url}/`}
+          render={() => <ListOfOrganizations userInfo={userInfo} />}
+        />
+        <Route
+          path={`${match.url}/addOrganization`}
+          render={() => <AddOrganization showMessage={showMessage} />}
+        />{' '}
+        <Route path={`${match.url}/login`} component={RedirectToAuthorization} />
+      </Switch>
+    </div>
   </div>
 );
 
