0a55f2f1c0721347a5c3fba14669cb3ce3c8108f
k.vaniuk <mmf.vanyukki@gmail.com>
2018-05-21 11:00:39 +0300

    Update menu

--- src/Components/Home/MainPage.js
+++ src/Components/Home/MainPage.js
@@ -8,7 +8,7 @@ class Home extends Component {
   render() {
     return (
 		<div className="main">
-		<Menu/>
+		<Menu name = 'main-menu'/>
 			<AboutUs/>
 			<Footer/>
 		</div>
--- src/Components/menu/Menu.jsx
+++ src/Components/menu/Menu.jsx
@@ -7,9 +7,9 @@ handlerClick = (e)=>{
 console.log(e.target)
 }
   render() {
-  	
+  	console.log(this.props)
     return (
-  <div className = 'menu'> 
+  <div className = {this.props.name}> 
       <Logo/>
       <MainList list = {[{name: 'о нас', url :'/'},{name: 'новости', url :'/news'}]} onClick = {this.handlerClick}/>
        <MediaList/>
