f3ba3e66d0e9a564b341ed9a6638f0185ac8e925
k.vaniuk <mmf.vanyukki@gmail.com>
2018-05-22 18:19:49 +0300

    Add validation email

--- src/Components/SubscribtionForm/SubscribtionForm.jsx
+++ src/Components/SubscribtionForm/SubscribtionForm.jsx
@@ -2,15 +2,52 @@ import React, { Component } from 'react';
 import './SubscribtionForm.css';
 import TextField from '../TextField/TextField';
 import Button from '../Button/Button';
+import Error from "../ErrorEmail/ErrorEmail";
+import '../ErrorEmail/ErrorEmail.css'
+
 
 class SubscribtionForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value:'',
+      error:true
+    }
+  }
+ onFocusInput = () => {
+   this.setState({error:true})
+ }
+  getValue = (str) => {
+    const newValue = str;
+    this.setState({value:newValue});
+  }
+
+  clickHandler = (e) => {
+    this.validation();
+  }
+
+  validation = () => {
+    const newValue = this.state.value;
+      this.setState({error: /[0-9a-z_]+@[0-9a-z_]+\.[a-z]{2,5}/i.test(newValue)})
+  }
+
   render() {
     return (
       <div className = 'subscribtion-form'> 
           <div className = 'wrapper-input'>
-            <TextField type = 'email' nameClass = 'input-email' placeholder = "Введите адрес электронной почты" name = 'email'/>
+         
+            <TextField 
+            type = 'email' 
+            nameClass = 'input-email' 
+            placeholder = "Введите адрес электронной почты" 
+            name = 'email' 
+            onChangeValue = {this.getValue}
+            onFocusInput = {this.onFocusInput} />
+            {!(this.state.error) ?  <Error />: null }
           </div>
-          <Button name = 'button-subscribe'  label = "подписаться"/>
+          <Button name = 'button-subscribe' 
+           label = "подписаться" 
+           clickHandler = {this.clickHandler}/>
       </div>
     );
   }
