48f5896276623e777a6e00b7b74407d0583571d4
k.vaniuk <mmf.vanyukki@gmail.com>
2018-06-04 14:39:52 +0300

    made alignment

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,11 +8,12 @@ class Footer extends Component {
 	}
   render() {
     return (
-  		<div className = 'footer-client'> 
-			<span className = 'copyright'>&#169; </span>
+			<div className = 'footer-client'> 
+				<span className = 'copyright'>&#169; </span>
 				<NavLink to="/" className = 'link-on-main-page'> on-info</NavLink>
-			<span>, {(new Date()).getFullYear()}</span>
-  		</div>
+				<span>, {(new Date()).getFullYear()}</span>
+		</div>
+  	
     );
   }
 }
