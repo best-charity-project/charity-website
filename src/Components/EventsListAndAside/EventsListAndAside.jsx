3e7c96be83ea8e084c7c0e339411c8d1a1099e35
k.vaniuk <mmf.vanyukki@gmail.com>
2018-06-06 14:22:21 +0300

    update events page

--- src/Components/EventsListAndAside/EventsListAndAside.jsx
+++ src/Components/EventsListAndAside/EventsListAndAside.jsx
@@ -1,22 +1,36 @@
 import React, {Component} from 'react';
-import NewsAside from '../NewsAside/NewsAside';
-import NewsList from '../NewsList/NewsList';
-import '../NewsListAndAside/NewsListAndAside.css'
+// import NewsAside from '../NewsAside/NewsAside';
+// import NewsList from '../NewsList/NewsList';
+// import '../NewsListAndAside/NewsListAndAside.css';
+import { server } from "../../api";
 class EventsListAndAside extends Component {
     state = {
         currentSourse:'Вce'
+    }
+    componentDidMount(){
+        this.getEventsList()
     }
     getCurrentSourse = (str) => {
         var newStr = str[0].toUpperCase() + str.slice(1);
         (newStr ==='Все')? this.setState({currentSourse:''}):this.setState({currentSourse:newStr});
     }
     render() {
+        console.log(this.state)
         return (
             <div className = 'aside-and-menu'>
-                <NewsAside getCurrentSourse = {this.getCurrentSourse}/>
-                <NewsList currentSourse = {this.state.currentSourse} name = "news-list"/>
+            Events
+                {/* <NewsAside getCurrentSourse = {this.getCurrentSourse}/>
+                <NewsList currentSourse = {this.state.currentSourse} name = "news-list"/> */}
             </div>
         ) 
+    }
+    getEventsList = () => {
+        fetch(`${ server }/events`)
+        .then(response => response.json())
+        .then(data => {         
+              this.setState({ events: data.events }              
+            )})
+        .catch(error => this.setState({ error, isLoading: false }));
     }
 }
 
