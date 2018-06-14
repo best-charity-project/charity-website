acf90a31cecea4bcc7b319056fce259425fed291
k.vaniuk <mmf.vanyukki@gmail.com>
2018-06-14 17:16:50 +0300

    fixed full news page

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -6,20 +6,21 @@ import SocialLinks from '../Menu/SocialLinks';
 class Menu extends Component {
   render() {
     return (
-  <div className ={'menu '+ this.props.name }> 
-      <Logo name = 'client-logo'/>
-      <MenuLinks
-       list = {[
-         {name: 'о нас', url :'/'},
-         {name: 'активности', url :'/'},
-         {name: 'новости', url :'/news'},
-         {name: 'проекты', url :'/projects'}
-         ]} 
-       onClick = {this.handlerClick}
-       classActive = 'active-link-client'
-       className = 'menu-links-client'/>
-       <SocialLinks />
-  </div>
+      <div className = 'menu-container'>
+          <div className ={'menu '+ this.props.name }> 
+              <Logo name = 'client-logo'/>
+               <MenuLinks
+                  list = {[
+                  {name: 'о нас', url :'/'},
+                    {name: 'новости', url :'/news'},
+                    {name: 'проекты', url :'/projects'}
+                    ]} 
+                  onClick = {this.handlerClick}
+                  classActive = 'active-link-client'
+                  className = 'menu-links-client'/>
+              <SocialLinks />
+          </div>
+        </div>
     );
   }
 }
