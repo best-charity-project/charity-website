0734398fdf3d62f5e8536234e6f6a87cc505eb72
Alexander Sorin <monkuver@gmail.com>
2018-06-15 19:15:09 +0300

    Fix issue with dropdown menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -4,24 +4,25 @@ import MenuLinks from '../Menu/MenuLinks';
 import SocialLinks from '../Menu/SocialLinks';
 
 class Menu extends Component {
-  render() {
-    return (
-  <div className ={'menu '+ this.props.name }> 
-      <Logo/>
-      <MenuLinks
-       list = {[
-         {name: 'о нас', url :'/'},
-         {name: 'активности', url :'/'},
-         {name: 'новости', url :'/news'},
-         {name: 'проекты', url :'/projects'}
-         ]} 
-       onClick = {this.handlerClick}
-       classActive = 'active-link-client'
-       className = 'menu-links-client'/>
-       <SocialLinks />
-  </div>
-    );
-  }
+    render() {
+        return (
+            <div className={'menu ' + this.props.name}>
+                <Logo />
+                <MenuLinks
+                    list={[
+                        { name: 'о нас', url: '/' },
+                        { name: 'активности', url: '/', dropdown: true },
+                        { name: 'новости', url: '/news' },
+                        { name: 'проекты', url: '/projects' },
+                    ]}
+                    onClick={this.handlerClick}
+                    classActive="active-link-client"
+                    className="menu-links-client"
+                />
+                <SocialLinks />
+            </div>
+        );
+    }
 }
 
 export default Menu;
