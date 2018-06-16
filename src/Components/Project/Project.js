9ea49a506b6444921922eede0e1eda3d13f25fd1
vadim_smirnov <smirnovvy@yahoo.com>
2018-06-16 21:08:40 +0300

    project component update

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import { server } from "../../api";
-import ProjectData from "../Project/ProjectData";
-
 
 class Project extends Component {
 
     render() {
         return (
-            <div className="projects-page-content">
-                <div className="img-container">
-                    <div className="img-placeholder"></div>
-                </div>
-                <div className="project-section">
-                    <div className="text-container">
-                        <div className="project-date"></div>
-                        <div className="project-title"></div>
-                        <div className="project-desc"></div>
-                        <button className="project-show-more-btn">читать далее</button>
-                    </div>
-                </div>
-            </div> 
+            <div>
+                {(this.props.projects) ? this.state.projects.map(projects => 
+                    <div className="projects-page-content">
+                        <div className="img-container">
+                            <div className="img-placeholder"></div>
+                        </div>
+                        <div className="project-section">
+                            <div className="text-container">
+                                <div className="project-date"></div>
+                                <div className="project-title"></div>
+                                <div className="project-desc"></div>
+                                <button className="project-show-more-btn">читать далее</button>
+                            </div>
+                        </div>
+                    </div> ) : null}   
+            </div>
         )
     }
 }
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { server } from "../../api";
+import axios from "axios";
 import AboutUs from "../../Components/AboutUs/AboutUs";
 import Footer from "../../Components/Footer/Footer";
 import Menu from "../../Components/Menu/Menu";
@@ -13,13 +15,27 @@ class Projects extends Component {
     constructor() {
         super();
         this.state = {
-            slideCount: 0
+            currentDisplayedProject: 0,
+            projects: []
         }
 
         this.nextProject = this.nextProject.bind(this);
         this.previousProject = this.previousProject.bind(this);
     }
     
+    componentDidMount() {
+            axios.get(`${server}/projects`)
+              .then(res => {
+                const projects = res.data;
+                this.setState({ projects });
+                console.log(res.projects)
+            })
+
+        this.setState({
+            currentDisplayedProject: 0  
+        })
+    }
+
     render() {
         return (
 			<div className="main-page-client">
