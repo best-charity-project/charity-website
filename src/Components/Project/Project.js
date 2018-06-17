1cc0575c203981a257a4410dd0f364209a237065
Alexander Sorin <monkuver@gmail.com>
2018-06-17 22:14:25 +0300

    Change logic for last and first items

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,33 +1,22 @@
 import React, { Component } from 'react';
 
 class Project extends Component {
-
     render() {
         return (
-                    <div className="projects-page-content">
-                        <div className="img-container">
-                            <div className="img-placeholder">
-                                {this.props.content.image}
-                            </div>
-                        </div>
-                        <div className="project-section">
-                            <div className="text-container">
-                                <div className="project-date">
-                                    {this.props.content.date}
-                                </div>
-                                <div className="project-title">
-                                    {this.props.content.name}
-                                </div>
-                                <div className="project-desc">
-                                    {this.props.content.shortText}
-                                </div>
-                                <button className="project-show-more-btn">
-                                    читать далее
-                                </button>
-                            </div>
-                        </div>
-                    </div> 
-        )
+            <div className="projects-page-content">
+                <div className="img-container">
+                    <div className="img-placeholder">{this.props.content.image}</div>
+                </div>
+                <div className="project-section">
+                    <div className="text-container">
+                        <div className="project-date">{this.props.content.date}</div>
+                        <div className="project-title">{this.props.content.name}</div>
+                        <div className="project-desc">{this.props.content.shortText}</div>
+                        <button className="project-show-more-btn">читать далее</button>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import { server } from "../../api";
-import axios from "axios";
+import { server } from '../../api';
+import axios from 'axios';
 import _ from 'lodash';
 import findIndex from 'lodash';
-import AboutUs from "../../Components/AboutUs/AboutUs";
-import Footer from "../../Components/Footer/Footer";
-import Menu from "../../Components/Menu/Menu";
-import "../Home/MainPage.css";
-import "../Projects/Projects.css";
-import Project from "../../Components/Project/Project";
-import SliderPreviousBtn from "../../Components/Slider/SliderButtons/SliderPreviousBtn";
-import SliderNextBtn from "../../Components/Slider/SliderButtons/SliderNextBtn"
-
+import AboutUs from '../../Components/AboutUs/AboutUs';
+import Footer from '../../Components/Footer/Footer';
+import Menu from '../../Components/Menu/Menu';
+import '../Home/MainPage.css';
+import '../Projects/Projects.css';
+import Project from '../../Components/Project/Project';
+import SliderPreviousBtn from '../../Components/Slider/SliderButtons/SliderPreviousBtn';
+import SliderNextBtn from '../../Components/Slider/SliderButtons/SliderNextBtn';
 
 class Projects extends Component {
     constructor() {
@@ -20,54 +19,56 @@ class Projects extends Component {
             currentDisplayedProject: {},
             currentProjectIndex: 0,
             projects: [],
-            isLastProject: false
-        }
+            isLastProject: false,
+            isFirstProject: true,
+        };
 
         this.nextProject = this.nextProject.bind(this);
         this.previousProject = this.previousProject.bind(this);
     }
-    
+
     componentDidMount() {
-        axios.get(`${server}/projects`)
-            .then(res => {
-                let { projects } = res.data;
-                this.setState({ projects });
-                this.setState({ 
-                    currentDisplayedProject: projects[0] 
-                });
+        axios.get(`${server}/projects`).then(res => {
+            let { projects } = res.data;
+            this.setState({ projects });
+            this.setState({
+                currentDisplayedProject: projects[0],
             });
+        });
     }
-    
-    
+
     render() {
         return (
-			<div className="main-page-client">
-                <Menu name = 'client-menu'/>
-                <Project content={this.state.currentDisplayedProject}/>
+            <div className="main-page-client">
+                <Menu name="client-menu" />
+                <Project content={this.state.currentDisplayedProject} />
                 <div className="projects-list-action-btns">
-                    <SliderPreviousBtn disabled={this.state.isLastProject} previousProject={this.previousProject}/>
-                    <SliderNextBtn disabled={this.state.isLastProject} nextProject={this.nextProject}/>          
+                    <SliderPreviousBtn disabled={this.state.isFirstProject} previousProject={this.previousProject} />
+                    <SliderNextBtn disabled={this.state.isLastProject} nextProject={this.nextProject} />
                 </div>
-                <Footer/>
-			</div>
+                <Footer />
+            </div>
         );
     }
 
     previousProject() {
-        let displayedProjectIndex = _.findIndex(this.state.projects, this.state.currentDisplayedProject)
+        let displayedProjectIndex = _.findIndex(this.state.projects, this.state.currentDisplayedProject);
         this.setState({
             currentDisplayedProject: this.state.projects[displayedProjectIndex - 1],
-            isLastProject: (this.state.projects.length - 1) === displayedProjectIndex
-        })
+            isFirstProject: displayedProjectIndex === 0,
+            isLastProject: false,
+        });
     }
 
     nextProject() {
-        let displayedProjectIndex = _.findIndex(this.state.projects, this.state.currentDisplayedProject)
+        let displayedProjectIndex = _.findIndex(this.state.projects, this.state.currentDisplayedProject);
+        console.log(this.state.projects.length);
         this.setState({
             currentDisplayedProject: this.state.projects[displayedProjectIndex + 1],
-            isLastProject: (this.state.projects.length - 1) === displayedProjectIndex
-        })
-        console.log(this.state.isLastProject)
+            isLastProject: this.state.projects.length - 1 === displayedProjectIndex,
+            isFirstProject: false,
+        });
+        console.log(this.state.isLastProject);
     }
 }
 
