45879f166b4a5d9ce82b650f0a11aa6b8cdcfbfd
Alexander Sorin <a.soryn@godeltech.com>
2018-06-18 15:43:53 +0300

    add menu file

--- src/Components/Menu/SocialLinks.jsx
+++ src/Components/Menu/SocialLinks.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
-class SocialLinks extends Component {	
+class SocialLinks extends Component {
     render() {
         return (
-            <div className = 'social-icons'> 
+            <div className="social-icons">
                 <ul>
-                    <li><a href = 'https://www.facebook.com/'></a></li>
-                    <li><a href = 'https://www.youtube.com/'></a></li>
-                    <li><a href = 'https://www.instagram.com/?hl=ru'></a></li>
-                    <li><a href = 'https://vk.com/'></a></li>
+                    <li>
+                        <a href="https://www.facebook.com/" />
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/" />
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/?hl=ru" />
+                    </li>
+                    <li>
+                        <a href="https://vk.com/" />
+                    </li>
                 </ul>
             </div>
         );
