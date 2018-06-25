626b1f52a6fc1f9bde4b9ce3d97dd8f3f0a85c0d
vadim_smirnov <smirnovvy@yahoo.com>
2018-06-25 15:09:52 +0300

    player add and minor improvements

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import ReactPlayer from 'react-player';
+import Organization from "../../Assets/AssetsSvg/organization.svg"; 
+import Person from "../../Assets/AssetsSvg/person.svg";
+import Phone from "../../Assets/AssetsSvg/phone.svg";
+
 
 class Project extends Component {
     constructor() {
@@ -30,14 +35,29 @@ class Project extends Component {
                 <div className="project-section">
                     <div className={this.state.fullTextView ? "full-text-card" : ""}>
                         <div className="text-container">
-                            <div className="project-date">
-                                {moment(this.props.content.date).format('Do MMMM YYYY')}
-                            </div>
-                            <div className="project-title">{this.props.content.name}</div>
+                            <div className="project-title"><h3>{this.props.content.name}</h3></div>
+                            <div className="project-address">{this.props.content.address}</div>
                             <div className="project-desc">
-                                <span dangerouslySetInnerHTML= {{__html: !this.state.isFullText ? this.props.content.shortText : this.props.content.fullText}}></span>
+                                <span dangerouslySetInnerHTML= {{__html: !this.state.isFullText ? null : this.props.content.fullText}}></span>
                             </div>
+                            {this.state.fullTextView && this.props.content.video != "" ? 
+                                <div className="player-wrapper">
+                                    <ReactPlayer  url={this.props.content.video} 
+                                              playing 
+                                              controls 
+                                              className="react-player" 
+                                              width='100%' 
+                                              height='100%'/>
+                                </div> : null }
                         </div>
+                        {this.state.fullTextView ? 
+                            <div className="contact-info">
+                                <p>Контактная информация</p>
+                                <hr/>
+                                <img className="contact-info-icon" src={Organization}></img><span>{this.props.content.organization}</span><br/>
+                                <img className="contact-info-icon" src={Person}></img><span>{this.props.content.head}</span><br/>
+                                <img className="contact-info-icon" src={Phone}></img><span>{this.props.content.contacts}</span><br/>
+                            </div> : null }
                     </div>
                     <div className="show-more-container">
                         <button 
