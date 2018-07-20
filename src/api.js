cffc5f7153d3fde4f16f72f13c8da43af59b2e1d
Alexander Sorin <a.soryn@godeltech.com>
2018-07-20 11:29:16 +0300

    dev issue

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import { baseURLDev } from './Configs/dev.json';
 import { baseURLProd } from './Configs/prod.json';
 
 let dev = process.env.NODE_ENV !== 'production';
-
+console.log('__comparing', dev);
+console.log('__env', process.env.NODE_ENV);
 // const prod = process.env.NODE_ENV !== 'development';
-
 
 export let server = dev ? baseURLDev : baseURLProd;
