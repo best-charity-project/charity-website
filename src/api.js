a4fdfdb74a904c88d33e13626db1df3fe40ffd93
frikeykir <kiryltsytsynov@gmail.com>
2018-07-20 13:45:47 +0300

    bugfix121

--- src/api.js
+++ src/api.js
@@ -2,9 +2,5 @@ import { baseURLDev } from './Configs/dev.json';
 import { baseURLProd } from './Configs/prod.json';
 
 let dev = process.env.NODE_ENV !== 'production';
-console.log('__comparing', dev);
-console.log('__env', process.env.NODE_ENV);
-console.log('__process', process.env);
-// const prod = process.env.NODE_ENV !== 'development';
 
 export let server = dev ? baseURLDev : baseURLProd;
