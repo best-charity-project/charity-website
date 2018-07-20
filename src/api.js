d67f64c52000b4749a5b58fadbe8ea66e51ff829
Alexander Sorin <a.soryn@godeltech.com>
2018-07-20 11:35:42 +0300

    d

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ import { baseURLProd } from './Configs/prod.json';
 let dev = process.env.NODE_ENV !== 'production';
 console.log('__comparing', dev);
 console.log('__env', process.env.NODE_ENV);
+console.log('__process', process.env);
 // const prod = process.env.NODE_ENV !== 'development';
 
 export let server = dev ? baseURLDev : baseURLProd;
