ffeca7bad546fcfb525c0ca944c57e1e05381f3d
Valeria Koliagina <milleriaim@gmail.com>
2018-08-01 13:52:31 +0300

    forum without posts and users

---
+++ src/Components/Admin/AdminComponents/AdminForum/AdminForumPost/AdminForumPost.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class AdminForumPost extends Component {
+    
+    render() {
+        return(
+            <div>    
+            </div>
+        )
+    }
+}
+export default AdminForumPost;
